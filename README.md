8a1879f33059be8a29d220cee3da7fdfe32f2995
Cody M. Jones <cjones490@gatech.edu>
2022-02-17 14:45:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# cmjones.gitlab.io
+
+This is my personal website hosted on GitLab Pages. It's written in plain HTML,
+CSS, and Javascript--no static website generators or JavaScript frameworks are
+used.
+
+# License
+
+All content is licensed under a [Creative Commons Attribution-ShareAlike 3.0 Unported License](https://creativecommons.org/licenses/by-sa/3.0/).
---
+++ public/js/scale.fix.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * Copyright (C) 2020 Cody M. Jones <codymjones@pm.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+var metas = document.getElementsByTagName('meta');
+var i;
+
+if (navigator.userAgent.match(/iPhone/i)) {
+  for (i = 0; i < metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+    }
+  }
+  document.addEventListener("gesturestart", gestureStart, false);
+}
+
+function gestureStart() {
+  for (i = 0; i < metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    }
+  }
+}
